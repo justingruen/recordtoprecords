90a1311b1ae91b70a747c014294893a263e1a83a
justingruen <fanestudios0@gmail.coim>
2020-11-27 16:28:01 -0500

    Updates

---
+++ precache-manifest.124aff33546df31d431f25e9834013e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0b609dfaf06eaf71322f8408f9e0db",
+    "url": "/justingruen/recordtoprecords/index.html"
+  },
+  {
+    "revision": "6af52c5bd7f0490895a1",
+    "url": "/justingruen/recordtoprecords/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e83a73e066797c60765d",
+    "url": "/justingruen/recordtoprecords/static/js/2.9a1c05d0.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/justingruen/recordtoprecords/static/js/2.9a1c05d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af52c5bd7f0490895a1",
+    "url": "/justingruen/recordtoprecords/static/js/main.681e3eb2.chunk.js"
+  },
+  {
+    "revision": "9c71e57753904d0aa29e",
+    "url": "/justingruen/recordtoprecords/static/js/runtime-main.294a6079.js"
+  },
+  {
+    "revision": "78c611a58e77f3664ce1a8eaadbecf57",
+    "url": "/justingruen/recordtoprecords/static/media/RTR_Logo.78c611a5.svg"
+  },
+  {
+    "revision": "3ebffe8e623cbf5e9cdc7fc7fd7db056",
+    "url": "/justingruen/recordtoprecords/static/media/record-player.3ebffe8e.jpg"
+  },
+  {
+    "revision": "e22e8d5e577c2b1869e69f03781a2a19",
+    "url": "/justingruen/recordtoprecords/static/media/turntable_1.e22e8d5e.jpg"
+  },
+  {
+    "revision": "799c89103fab26a34744c79fdd9dd539",
+    "url": "/justingruen/recordtoprecords/static/media/turntable_2.799c8910.jpg"
+  },
+  {
+    "revision": "8aff9809133f4c5fe9a0f1fabb105cee",
+    "url": "/justingruen/recordtoprecords/static/media/vinyl_1.8aff9809.jpg"
+  }
+]);
